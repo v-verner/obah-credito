94f6424be1e5b448de41e079bf0d64110bdc3537
Guilherme <gdilkin@yahoo.com.br>
2022-07-08 09:30:53 -0300

    Inserção de TODO no ajax

--- app/controller/ajax.php
+++ app/controller/ajax.php
@@ -9,6 +9,16 @@ function ajaxCreateSimulation(): void
     // NOTE
     // Ao finalizar a simulação, salvar ela no post-type "simulation" e retornar no ajax o ID dela futuras atualizações (saveSimulation())
 
+    // TODO
+    // 1 gerar simulação no 99cred
+    // 2 enviar lead para o bitrix (aguardar)
+    // 3 salvar simulação
+    // 4 gerar hash (base64_encode) do ID da simulação salva
+    // 5 retornar url da página "simulador" com o hash gerado 
+    //     5.1 criar rewrite para hashes no simulador
+
+
+
     wp_send_json_success();
 }
 
@@ -16,7 +26,7 @@ add_action('wp_ajax_obah/update_simulation', 'ajaxUpdateSimulation');
 add_action('wp_ajax_nopriv_obah/update_simulation', 'ajaxUpdateSimulation');
 function ajaxUpdateSimulation(): void
 {
-    check_ajax_referer('obah/create_simulation');
+    check_ajax_referer('obah/update_simulation');
 
     // NOTE
     // Carregar os dados da simulação anterior para uso na simulação (loadSimulation())
