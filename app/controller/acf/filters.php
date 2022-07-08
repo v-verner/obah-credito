b6020c439ff6b6a22a5750838f0e8ead149b176f
v-verner <vini.verner1@gmail.com>
2022-07-08 08:58:29 -0300

    grupo de campos

--- app/controller/acf/filters.php
+++ app/controller/acf/filters.php
@@ -1,11 +1,11 @@
 <?php defined('ABSPATH') || exit(); 
 
 add_filter('acf/settings/save_json', function($path){
-   return __DIR__.DIRECTORY_SEPARATOR.'json';
+   return __DIR__. '/json';
 });
 
 add_filter('acf/settings/load_json', function($paths){
-   $paths[] = __DIR__.DIRECTORY_SEPARATOR.'json';
+   $paths[] = __DIR__. '/json';
    return $paths;
 });
 
