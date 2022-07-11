545c9051b6a17f528e0ce38f6874f728e663498b
jaques-l <jaques.l@outlook.com>
2022-07-11 10:23:18 -0300

    Adição de overflow na table do mobile em simulador

--- app/views/page/simulador.php
+++ app/views/page/simulador.php
@@ -233,90 +233,94 @@
                     </div>
 
                     <div class="col large-12 hide-for-large">
-                        <table>
-                            <tr>
-                                <th>seleção</th>
-                                <td class="text-center"><input type="radio" name="bank-select" id=""></td>
-                                <td class="text-center"><input type="radio" name="bank-select" id=""></td>
-                                <td class="text-center"><input type="radio" name="bank-select" id=""></td>
-                                <td class="text-center"><input type="radio" name="bank-select" id=""></td>
+                        <div class="responsive-scroll hide-for-large">
+                            <table>
+                                <tr>
+                                    <th>seleção</th>
+                                    <td class="text-center"><input type="radio" name="bank-select" id=""></td>
+                                    <td class="text-center"><input type="radio" name="bank-select" id=""></td>
+                                    <td class="text-center"><input type="radio" name="bank-select" id=""></td>
+                                    <td class="text-center"><input type="radio" name="bank-select" id=""></td>
 
-                            </tr>
-                            <tr>
-                                <th>banco</th>
-                                <td class="bank-img"><img width="62" height="62"
-                                        src="<?= VVerner\Assets::getInstance()->getImageFileUrl('logo-caixa-economica-federal.png') ?>"
-                                        alt="Caixa"></td>
-                                <td class="bank-img"><img width="62" height="62"
-                                        src="<?= VVerner\Assets::getInstance()->getImageFileUrl('logo-itau.png') ?>"
-                                        alt="Itaú"></td>
-                                <td class="bank-img"><img width="62" height="62"
-                                        src="<?= VVerner\Assets::getInstance()->getImageFileUrl('logo-caixa-economica-federal.png') ?>"
-                                        alt="Caixa"></td>
-                                <td class="bank-img"><img width="62" height="62"
-                                        src="<?= VVerner\Assets::getInstance()->getImageFileUrl('logo-itau.png') ?>"
-                                        alt="Itaú"></td>
-                            </tr>
-                            <tr>
-                                <th><p class="mb-0">valor</p><p class="mb-0">financiado</p></th>
-                                <td class="text-center"><p>R$120.000</p></td>
-                                <td class="text-center"><p>R$120.000</p></td>
-                                <td class="text-center"><p>R$120.000</p></td>
-                                <td class="text-center"><p>R$120.000</p></td>
-                            </tr>
-                            <tr>
-                                <th>entrada</th>
-                                <td class="text-center"><p>R$120.000</p>
-                                </td>
-                                <td class="text-center"><p>R$120.000</p></td>
-                                <td class="text-center"><p>R$120.000</p></td>
-                                <td class="text-center"><p>R$120.000</p></td>
-                            </tr>
-                            <tr>
-                                <th>prazo</th>
-                                <td class="text-center"><p>320</p></td>
-                                <td class="text-center"><p>320</p></td>
-                                <td class="text-center"><p>320</p></td>
-                                <td class="text-center"><p>320</p></td>
-                            </tr>
-                            <tr>
-                                <th><p class="mb-0">primeira</p><p class="mb-0">parcela</p></th>
-                                <td class="text-center"><p>R$120.000</p></td>
-                                <td class="text-center"><p>R$120.000</p></td>
-                                <td class="text-center"><p>R$120.000</p></td>
-                                <td class="text-center"><p>R$120.000</p></td>
-                            </tr>
-                            <tr>
-                                <th><p class="mb-0">última</p><p class="mb-0">parcela</p></th>
-                                <td class="text-center"><p>R$120.000</p></td>
-                                <td class="text-center"><p>R$120.000</p></td>
-                                <td class="text-center"><p>R$120.000</p></td>
-                                <td class="text-center"><p>R$120.000</p></td>
-                            </tr>
-                            <tr>
-                                <th><p class="mb-0">renda</p><p class="mb-0">mínima</p></th>
-                                <td class="text-center"><p>R$120.000</p></td>
-                                <td class="text-center"><p>R$120.000</p></td>
-                                <td class="text-center"><p>R$120.000</p></td>
-                                <td class="text-center"><p>R$120.000</p></td>
-                            </tr>
-                            <tr>
-                                <th>taxa efetiva</th>
-                                <td class="text-center">8,89% A.A</td>
-                                <td class="text-center">8,89% A.A</td>
-                                <td class="text-center">8,89% A.A</td>
-                                <td class="text-center">8,89% A.A</td>
-                            </tr>
-                            <tr>
-                                <th><p class="mb-0">sistema de</p><p class="mb-0">amortização</p></th>
-                                <td class="text-center"><p>SAC</p></td>
-                                <td class="text-center"><p>SAC</p></td>
-                                <td class="text-center"><p>SAC</p></td>
-                                <td class="text-center"><p>SAC</p></td>
-                            </tr>
+                                </tr>
+                                <tr>
+                                    <th>banco</th>
+                                    <td class="bank-img"><img width="62" height="62"
+                                            src="<?= VVerner\Assets::getInstance()->getImageFileUrl('logo-caixa-economica-federal.png') ?>"
+                                            alt="Caixa"></td>
+                                    <td class="bank-img"><img width="62" height="62"
+                                            src="<?= VVerner\Assets::getInstance()->getImageFileUrl('logo-itau.png') ?>"
+                                            alt="Itaú"></td>
+                                    <td class="bank-img"><img width="62" height="62"
+                                            src="<?= VVerner\Assets::getInstance()->getImageFileUrl('logo-caixa-economica-federal.png') ?>"
+                                            alt="Caixa"></td>
+                                    <td class="bank-img"><img width="62" height="62"
+                                            src="<?= VVerner\Assets::getInstance()->getImageFileUrl('logo-itau.png') ?>"
+                                            alt="Itaú"></td>
+                                </tr>
+                                <tr>
+                                    <th><p class="mb-0">valor</p><p class="mb-0">financiado</p></th>
+                                    <td class="text-center"><p>R$120.000</p></td>
+                                    <td class="text-center"><p>R$120.000</p></td>
+                                    <td class="text-center"><p>R$120.000</p></td>
+                                    <td class="text-center"><p>R$120.000</p></td>
+                                </tr>
+                                <tr>
+                                    <th>entrada</th>
+                                    <td class="text-center"><p>R$120.000</p>
+                                    </td>
+                                    <td class="text-center"><p>R$120.000</p></td>
+                                    <td class="text-center"><p>R$120.000</p></td>
+                                    <td class="text-center"><p>R$120.000</p></td>
+                                </tr>
+                                <tr>
+                                    <th>prazo</th>
+                                    <td class="text-center"><p>320</p></td>
+                                    <td class="text-center"><p>320</p></td>
+                                    <td class="text-center"><p>320</p></td>
+                                    <td class="text-center"><p>320</p></td>
+                                </tr>
+                                <tr>
+                                    <th><p class="mb-0">primeira</p><p class="mb-0">parcela</p></th>
+                                    <td class="text-center"><p>R$120.000</p></td>
+                                    <td class="text-center"><p>R$120.000</p></td>
+                                    <td class="text-center"><p>R$120.000</p></td>
+                                    <td class="text-center"><p>R$120.000</p></td>
+                                </tr>
+                                <tr>
+                                    <th><p class="mb-0">última</p><p class="mb-0">parcela</p></th>
+                                    <td class="text-center"><p>R$120.000</p></td>
+                                    <td class="text-center"><p>R$120.000</p></td>
+                                    <td class="text-center"><p>R$120.000</p></td>
+                                    <td class="text-center"><p>R$120.000</p></td>
+                                </tr>
+                                <tr>
+                                    <th><p class="mb-0">renda</p><p class="mb-0">mínima</p></th>
+                                    <td class="text-center"><p>R$120.000</p></td>
+                                    <td class="text-center"><p>R$120.000</p></td>
+                                    <td class="text-center"><p>R$120.000</p></td>
+                                    <td class="text-center"><p>R$120.000</p></td>
+                                </tr>
+                                <tr>
+                                    <th><p class="mb-0">taxa</p><p class="mb-0">efetiva</p></th>
+                                    <td class="text-center">8,89% A.A</td>
+                                    <td class="text-center">8,89% A.A</td>
+                                    <td class="text-center">8,89% A.A</td>
+                                    <td class="text-center">8,89% A.A</td>
+                                </tr>
+                                <tr>
+                                    <th><p class="mb-0">sistema de</p><p class="mb-0">amortização</p></th>
+                                    <td class="text-center"><p>SAC</p></td>
+                                    <td class="text-center"><p>SAC</p></td>
+                                    <td class="text-center"><p>SAC</p></td>
+                                    <td class="text-center"><p>SAC</p></td>
+                                </tr>
 
-                        </table>
+                            </table>
+                        </div>
                     </div>
+                        
+
                     <div class="col pb-0 close-col large-6 small-6">
                         <button class="button mb-0 pb-0">
                             <i class="icon-angle-left"></i>
