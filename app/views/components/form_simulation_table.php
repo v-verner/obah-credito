5f879b5e6fae4f1df3b3bc132c0495a5569bc3ca
jaques-l <jaques.l@outlook.com>
2022-07-14 10:45:40 -0300

    correção de erro formulario de simulação

--- app/views/components/form_simulation_table.php
+++ app/views/components/form_simulation_table.php
@@ -3,120 +3,9 @@
 <?php $currentSimulation = $simulationResults[0] ?>
 
 <div class="hide-for-medium hide-for-small">
-        <div class="responsive-scroll">
-            <table>
-                <tr class="table-title">
-                    <th class="text-center">seleção</th>
-                    <th class="text-center">banco</th>
-                    <th class="text-center">
-                        <p class="mb-0">valor</p>
-                        <p class="mb-0">financiado</p>
-                    </th>
-                    <th class="text-center">entrada</th>
-                    <th class="text-center">prazo</th>
-                    <th class="text-center">
-                        <p class="mb-0">primeira</p>
-                        <p class="mb-0">parcela</p>
-                    </th>
-                    <th class="text-center">
-                        <p class="mb-0">última</p>
-                        <p class="mb-0">parcela</p>
-                    </th>
-                    <th class="text-center">
-                        <p class="mb-0">renda</p>
-                        <p class="mb-0">mínima</p>
-                    </th>
-                    <th class="text-center">
-                        <p class="mb-0">sistema de</p>
-                        <p class="mb-0">amortização</p>
-                    </th>
-                    <th class="text-center">taxa efetiva</th>
-                </tr>
-                
-                <?php foreach($currentSimulation as $bank) :?>
-                    <?php $title = explode('-', trim($bank->Titulo)); ?>
-                    <?php $title = trim(array_shift($title)); ?>
-                    <tr class="bank">
-                        <td class="text-center"><input type="radio" name="bank-select" id=""></td>
-                        <td style="text-transform: uppercase">
-                        <img width="62" height="62"
-                                src="<?= $bank->logo_banco ?>"
-                                alt="Caixa">
-                        <span title="<?= $bank->Titulo ?>"><?= $title ?></span>
-                        </td>  
-                        <td class="text-center">
-                            <p>R$<?= number_format($bank->_Valor_Financiado, '2', ',' , '.') ?></p>
-                        </td>
-                        <td class="text-center">
-                            <p>R$<?= number_format($bank->_Valor_Entrada, '2', ',' , '.') ?></p>
-                        </td>
-                        <td class="text-center">
-                            <p><?= $bank->_Prazo ?></p>
-                        </td>
-                        <td class="text-center">
-                            <p>R$<?= number_format($bank->Valor_Primeira_Parcela, '2', ',' , '.') ?></p>
-                        </td>
-                        <td class="text-center">
-                            <p>R$<?= number_format($bank->Valor_Ultima_Parcela, '2', ',' , '.') ?></p>
-                        </td>
-                        <td class="text-center">
-                            <p>R$<?= number_format($bank->Valor_Renda_Minima, '2', ',' , '.') ?></p>
-                        </td>
-                        <td class="text-center">
-                            <p><?= $bank->Modalidade ?></p>
-                        </td>
-                        <td class="text-center"><?= $bank->Taxa_Nominal ?>% A.A</td>
-                    </tr>    
-                <?php endforeach;?>  
-                                              
-            </table>
-        </div>
-    </div>
-
-    <div class="col large-12 hide-for-large text-left">
-        <span class="scroll-for-more-text">Arraste para o lado</span>
-        <i class="icon-angle-right scroll-for-more"></i>
-
-        <div class="responsive-scroll">
-            <table>
-                <tr>
-                    <th>seleção</th>
-                    <td class="text-center"><input type="radio" name="bank-select" id=""></td>
-                    <td class="text-center"><input type="radio" name="bank-select" id=""></td>
-                    <td class="text-center"><input type="radio" name="bank-select" id=""></td>
-                    <td class="text-center"><input type="radio" name="bank-select" id=""></td>
-
-                </tr>
-                <tr>
-                    <th>banco</th>
-                    <td class="bank-img"><img width="62" height="62"
-                            src="<?= VVerner\Assets::getInstance()->getImageFileUrl('logo-caixa-economica-federal.png') ?>"
-                            alt="Caixa"></td>
-                    <td class="bank-img"><img width="62" height="62"
-                            src="<?= VVerner\Assets::getInstance()->getImageFileUrl('logo-itau.png') ?>"
-                            alt="Itaú"></td>
-                    <td class="bank-img"><img width="62" height="62"
-                            src="<?= VVerner\Assets::getInstance()->getImageFileUrl('logo-caixa-economica-federal.png') ?>"
-                            alt="Caixa"></td>
-                    <td class="bank-img"><img width="62" height="62"
-                            src="<?= VVerner\Assets::getInstance()->getImageFileUrl('logo-itau.png') ?>"
-                            alt="Itaú"></td>
-                </tr>
-                <tr>
-                    <th><p class="mb-0">valor</p><p class="mb-0">financiado</p></th>
-                    <td class="text-center"><p>R$120.000</p></td>
-                    <td class="text-center"><p>R$120.000</p></td>
-                    <td class="text-center"><p>R$120.000</p></td>
-                    <td class="text-center"><p>R$120.000</p></td>
-                </tr>
-                <tr>
-                    <th>entrada</th>
-                    <td class="text-center"><p>R$120.000</p>
-<div class="col large-12  hide-for-medium hide-for-small">
     <div class="responsive-scroll">
         <table>
             <tr class="table-title">
-                <!-- <th class="text-center">seleção</th> -->
                 <th class="text-center">banco</th>
                 <th class="text-center">
                     <p class="mb-0">valor</p>
@@ -147,7 +36,6 @@
                 <?php $title = explode('-', trim($bank->Titulo)); ?>
                 <?php $title = trim(array_shift($title)); ?>
                 <tr class="bank">
-                    <!-- <td class="text-center"><input type="radio" name="bank-select" id=""></td> -->
                     <td style="text-transform: uppercase">
                     <img width="62" height="62"
                             src="<?= $bank->logo_banco ?>"
@@ -182,17 +70,13 @@
         </table>
     </div>
 </div>
-
 <div class="col large-12 hide-for-large text-left">
     <span class="scroll-for-more-text">Arraste para o lado</span>
     <i class="icon-angle-right scroll-for-more"></i>
 
     <div class="responsive-scroll">
         <table>
-            <!-- <tr>
-                <th>seleção</th>
-                <td class="text-center"><input type="radio" name="bank-select" id=""></td>
-            </tr> -->
+            
             <tr>
                 <th>banco</th>
                 <?php foreach($currentSimulation as $bank) :?>
@@ -209,7 +93,7 @@
             <tr>
                 <th><p class="mb-0">valor</p><p class="mb-0">financiado</p></th>
                 <?php foreach($currentSimulation as $bank) :?>
-                <td class="text-center"><p>R$<?= number_format($bank->_Valor_Financiado, '2', ',' , '.') ?></p>p</td>
+                <td class="text-center"><p>R$<?= number_format($bank->_Valor_Financiado, '2', ',' , '.') ?></p></td>
                 <?php endforeach;?>
             </tr>
             <tr>
