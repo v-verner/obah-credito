107846d01cbc51fa89ea93ec4fa1d1dd2e0038df
Guilherme <gdilkin@yahoo.com.br>
2022-07-20 10:06:17 -0300

    Ajuste dos icon box home

--- app/views/page/home.php
+++ app/views/page/home.php
@@ -1,117 +1,71 @@
+<?php
+$features = [
+    'Simulação on-line em todos os bancos.',
+    'Atendimento desde o crédito até o registro.',
+    'Foco no financiamento imobiliário, facilidades para compradores, vendedores e instituição financeira.',
+    'Avaliamos seu crédito em até 24h',
+    'Consultores especializados',
+    'Consultores especializados',
+    'Processo 100% gratuito'
+];
+?>
 <section class="section dark home hero">
     <div class="col large-12">        
         <div class="row row-large">
             <div class="col large-8">
+
                 <div class="row">
                     <div class="col large-12">
                         <h1>Obah Crédito Correspondente Bancário</h1>
                     </div>
                     <div class="col large-2 medium-2 small-3">
-                        <img class="banks-img" src="<?= VVerner\Assets::getInstance()->getImageFileUrl('logo-itau.png') ?>" alt="Itaú">
+                        <img class="banks-img mb-half" src="<?= VVerner\Assets::getInstance()->getImageFileUrl('logo-itau.png') ?>" alt="Itaú">
                     </div>
                     <div class="col large-2 medium-2 small-3">
-                        <img class="banks-img" src="<?= VVerner\Assets::getInstance()->getImageFileUrl('logo-santander.png') ?>" alt="Santander">
+                        <img class="banks-img mb-half" src="<?= VVerner\Assets::getInstance()->getImageFileUrl('logo-santander.png') ?>" alt="Santander">
                     </div>
                     <div class="col large-2 medium-2 small-3">
-                        <img class="banks-img" src="<?= VVerner\Assets::getInstance()->getImageFileUrl('logo-caixa-economica-federal.png') ?>" alt="Caixa">
+                        <img class="banks-img mb-half" src="<?= VVerner\Assets::getInstance()->getImageFileUrl('logo-caixa-economica-federal.png') ?>" alt="Caixa">
                     </div>
                     <div class="col large-2 medium-2 small-3">
-                        <img class="banks-img" src="<?= VVerner\Assets::getInstance()->getImageFileUrl('logo-bradesco.png') ?>" alt="Bradesco">
+                        <img class="banks-img mb-half" src="<?= VVerner\Assets::getInstance()->getImageFileUrl('logo-bradesco.png') ?>" alt="Bradesco">
                     </div>
                 </div>
-            </div>
-        </div>
-        <div class="row row-large">
-            <div class="col large-4">                
-                <div class="icon-box featured-box icon-box-left text-left">
-                    <div class="icon-box-img">
-                        <div class="icon">
-                            <div class="icon-inner">
-                                <i class="icon-checkmark"></i>
+
+                <?php foreach ($features as $k => $v) : ?>
+                    <?php if ($k % 2 === 0): ?>
+                        <div class="row row-large">
+                    <?php endif; ?>
+
+                        <div class="col small-12 medium-6">
+                            <div class="col-inner">
+                                <div class="icon-box icon-box-left text-left">
+                                    <div class="icon-box-img">
+                                        <div class="icon">
+                                            <div class="icon-inner">
+                                                <i class="icon-checkmark"></i>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div class="icon-box-text last-reset">
+                                        <h3 class="thin-font">
+                                            <?= $v ?>
+                                        </h3>
+                                    </div>
+                                </div>
                             </div>
                         </div>
-                    </div>
-                    <div class="icon-box-text last-reset">
-                        <h3 class="thin-font">Simulação on-line em todos os bancos</h3>
-                    </div>
-                </div>
-                <div class="icon-box featured-box icon-box-left text-left">
-                    <div class="icon-box-img">
-                        <div class="icon">
-                            <div class="icon-inner">
-                                <i class="icon-checkmark"></i>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="icon-box-text last-reset">
-                    <h3 class="thin-font">Atendimento desde o crédito até o registro</h3>
-                    </div>
-                </div>
-                <div class="icon-box featured-box hide-for-small hide-for-medium icon-box-left text-left">
-                    <div class="icon-box-img">
-                        <div class="icon">
-                            <div class="icon-inner">
-                                <i class="icon-checkmark"></i>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="icon-box-text last-reset">
-                        <h3 class="thin-font">Foco no financiamento imobiliário, facilidades para compradores, vendedores e instituição financeira.</h3>
-                    </div>
-                </div>
-                <button class="button go-to-simulation-button">
-                    <a href="<?= get_permalink(getObahPageId('formulário')) ?>">Faça uma simulação</a>
-                </button>
-            </div>
-            <div class="col large-4">
-                <div class="icon-box featured-box icon-box-left text-left">
-                    <div class="icon-box-img">
-                        <div class="icon">
-                            <div class="icon-inner">
-                                <i class="icon-checkmark"></i>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="icon-box-text last-reset">
-                        <h3 class="thin-font">Avaliamos seu crédito em até 24h</h3>
-                    </div>
-                </div>
-                <div class="icon-box featured-box icon-box-left text-left">
-                    <div class="icon-box-img">
-                        <div class="icon">
-                            <div class="icon-inner">
-                                <i class="icon-checkmark"></i>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="icon-box-text last-reset">
-                        <h3 class="thin-font">Consultores especializados</h3>
-                    </div>
-                </div>
-                <div class="icon-box featured-box icon-box-left text-left">
-                    <div class="icon-box-img">
-                        <div class="icon">
-                            <div class="icon-inner">
-                                <i class="icon-checkmark"></i>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="icon-box-text last-reset">
-                        </i><h3 class="thin-font">Processo 100% gratuito</h3>
-                    </div>
-                </div>
-                <div class="icon-box featured-box hide-for-large icon-box-left text-left">
-                    <div class="icon-box-img">
-                        <div class="icon">
-                            <div class="icon-inner">
-                                <i class="icon-checkmark"></i>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="icon-box-text last-reset">
-                        <h3 class="thin-font">Foco no financiamento imobiliário, facilidades para compradores, vendedores e instituição financeira.</h3>
-                    </div>
-                </div>
+
+                    <?php if ($k % 2 !== 0): ?>
+                        </div> <!-- row row-large -->
+                    <?php endif; ?>
+
+                <?php endforeach; ?>
+
+                <?php if ($k % 2 === 0): ?>
+                    </div> <!-- row row-large -->
+                <?php endif; ?>
+
             </div>
         </div>
     </div>
